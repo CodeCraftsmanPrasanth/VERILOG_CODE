28c469f1fca193cd1c0fdf74632faa31c164258d
Prasanth <162542956+CodeCraftsmanPrasanth@users.noreply.github.com>
2025-08-02 20:26:25 +0530

    Update and rename Vector0.V to Vector0.v

--- HDL_BITS/VERILOG_LANGUAGE/VECTORS/Vector0.v
+++ HDL_BITS/VERILOG_LANGUAGE/VECTORS/Vector0.v
@@ -4,5 +4,5 @@ module top_module (
     output wire o2,
     output wire o1,
     output wire o0  ); // Module body starts after module declaration
-
+    assign {outv[2:0],o2,o1,o0}={vec[2:0],vec[2],vec[1],vec[0]};
 endmodule
