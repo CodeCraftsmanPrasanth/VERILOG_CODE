062d96031b881e7efbb203a7fad016d9f945b52c
Prasanth <162542956+CodeCraftsmanPrasanth@users.noreply.github.com>
2025-08-14 19:51:33 +0530

    Delete HDL_BITS directory

--- HDL_BITS/VERILOG_LANGUAGE/VECTORS/Vector0.v
+++
@@ -1,8 +0,0 @@
-module top_module ( 
-    input wire [2:0] vec,
-    output wire [2:0] outv,
-    output wire o2,
-    output wire o1,
-    output wire o0  ); // Module body starts after module declaration
-    assign {outv[2:0],o2,o1,o0}={vec[2:0],vec[2],vec[1],vec[0]};
-endmodule
